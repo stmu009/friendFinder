37a864e3d3ebec5ed10db3cea9f7da58ff4ae121
Damian <dramirez@petmate.com>
2018-05-25 19:35:53 -0500

    updated friends data and specified engine

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,18 +1,82 @@
 var friends = [{
-    "name": "Ahmed",
-    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "name": "Abraham Lincoln",
+    "photo": "https://www.biographyonline.net/images/150/250-Abraham_Lincoln.jpg",
     "scores": [
-        5,
         1,
-        4,
-        4,
-        5,
         1,
-        2,
-        5,
-        4,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
         1
     ]
-}];
+},
+{
+    "name": "Mother Teresa",
+    "photo": "https://www.biographyonline.net/wp-content/uploads/2014/05/MotherTeresa_0902.jpg",
+    "scores": [
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2,
+        2
+    ]
+},
+{
+    "name": "John F. Kennedy",
+    "photo": "https://www.biographyonline.net/wp-content/uploads/2014/05/John_F._Kennedy1.jpg",
+    "scores": [
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3,
+        3
+    ]
+},
+{
+    "name": "Martin Luther King",
+    "photo": "https://www.biographyonline.net/wp-content/uploads/2014/05/Martin_Luther_King_Jr2.jpg",
+    "scores": [
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4,
+        4
+    ]
+},
+{
+    "name": "Bill Gates",
+    "photo": "https://www.biographyonline.net/wp-content/uploads/2014/05/Bill_Gates_in_WEF_20071.jpg",
+    "scores": [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5
+    ]
+},];
 
 module.exports = friends;
