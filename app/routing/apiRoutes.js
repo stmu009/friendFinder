e6ed1083ae4aa13017ddd3ad280fa2de58146e79
Damian <dramirez@petmate.com>
2018-05-25 19:07:29 -0500

    finished post and compatability score comparison

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,42 +4,43 @@ module.exports = function (app) {
     // A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
     app.get("/api/friends", function (request, response) {
-        console.log("List all friends");
+        // console.log("List all friends");
         response.json(friends)
     })
 
     // A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic. 
     app.post('/api/friends', (req, res) => {
-        console.log("friends:", friends);
-        console.log("friends type:", typeof friends);
+        // console.log("friends:", friends);
+        var bestMatch = {
+            name: "",
+            photo: "",
+            friendDifference: Infinity
+        };
+
 
         // loop through friends and get compatability score
         friends.forEach(friend => {
+            absDiff = 0;
             friend.scores.forEach((questionScore, questionNum) => {
-                console.log("Question Score", questionScore);
-                console.log("index:", questionNum);
+                // console.log("Question Score", questionScore);
+                // console.log("index:", questionNum);
+
                 // get the difference for each score
-                
+                // console.log("my score:", req.body.scores[questionNum]);
+                // console.log("friend score:", questionScore)
+                absDiff += Math.abs(parseInt(req.body.scores[questionNum]) - parseInt(questionScore));
+                // console.log('absolute difference:', absDiff);
             });
+
+            if (absDiff <= bestMatch.friendDifference) {
+                bestMatch.name = friend.name;
+                bestMatch.photo = friend.photo;
+                bestMatch.friendDifference = absDiff;
+            }
         });
 
         friends.push(req.body);
-        res.json(friends[0]);
+        res.json(bestMatch);
     });
 
 };
-
-
-
-
-// Determine the user's most compatible friend using the following as a guide:
-// Convert each user's results into a simple array of numbers (ex: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]).
-// With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the totalDifference.
-// Example: 
-// User 1: [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
-// User 2: [3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
-// Total Difference: 2 + 1 + 2 = 5
-// Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both 5-3 and 3-5 as 2, and so on. 
-// The closest match will be the user with the least amount of difference.
-// Once you've found the current user's most compatible friend, display the result as a modal pop-up.
-// The modal should display both the name and picture of the closest match.
